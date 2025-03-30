5579b1587551cc53390df6a91aeda23655739083
Dylan J. Gerrits <gerritsdylan@gmail.com>
2025-03-30 13:03:09 +0200

    🧹 Remove JavaScript files

--- main.js
+++
@@ -1,7 +0,0 @@
-import lex from './lexer';
-import parse from './parser';
-const code = "1 + 2 * 3 - 4 / 5";
-const tokens = lex(code);
-console.log("Tokens:", tokens);
-const ast = parse(tokens);
-console.log("AST:", ast);
